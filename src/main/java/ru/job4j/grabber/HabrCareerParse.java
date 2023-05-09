3610f7ff8be66b34c431538c21ab750845e7181c
Илья <ily470@yandex.ru>
2023-05-09 18:51:38 +0700

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -31,6 +31,13 @@ public class HabrCareerParse {
         });
     }
 
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements description = document.select(".vacancy-description__text");
+        return String.format("%s%n", description.text());
+    }
+
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= 5; i++) {
             String link = "%s%d".formatted(PAGE_LINK, i);
